213a5ab556386e7eec22e26f54e2e63772b2dae3
EK-Choi <77577929+EK-Choi@users.noreply.github.com>
2021-03-04 20:55:11 +0900

    최종

--- app.py
+++ app.py
@@ -18,14 +18,80 @@ SECRET_KEY = 'SPARTA'
 client = MongoClient('localhost', 27017)
 db = client.dbsparta_plus_week4
 
-# 랜딩페이지 index.html
-
-
+#------------------------------------------------------랜딩페이지 index.html------------------------------------------------------
 @app.route('/')
-def mypage():
-    return render_template('index.html')
+def getMain():
+    return render_template('/index.html')
+
+# ------------------------------------------------------로그인/회원가입------------------------------------------------------
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('login.html', msg=msg)
+    
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    # 회원가입
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    # PW해시(암호화)
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    doc = {
+        "username": username_receive,                               # 아이디
+        "password": password_hash,                                  # 비밀번호
+        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
+        "profile_pic": "",                                          # 프로필 사진 파일 이름
+        "profile_pic_real": "profile_pics/profile_placeholder.png", # 프로필 사진 기본 이미지
+        "profile_info": ""                                          # 프로필 한 마디
+    }
+    # DB 저장
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+#  ID중복확인
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+# ------------------------------------------------------메인 페이지 상위 15개 전시------------------------------------------------------
+@app.route('/info', methods=['GET'])
+def read_info():
+    all_info = list(db.exhibition.find({}, {'_id': False}))
+    return jsonify({'all_info': all_info})
+
+# ------------------------------------------------------인기 전시------------------------------------------------------
+@app.route('/show', methods=['GET'])
+def show():
+    shows = list(db.exhibition.find({}, {'_id': False}))
+    return render_template("show.html", shows=shows)
+
+
+# ------------------------------------------------------마이페이지------------------------------------------------------
 @app.route('/mypage')  # 마이페이지 연결
 def mypage_diary():
     return render_template('mypage.html')
@@ -133,83 +199,6 @@ def delete_diary():
     file_receive = request.form['file_give']
     db.diary.delete_one({'file': file_receive})
     return jsonify({'msg': '삭제 완료!'})
-
-
-@app.route('/login')
-def login():
-    msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
-
-
-@app.route('/sign_in', methods=['POST'])
-def sign_in():
-    # 로그인
-    username_receive = request.form['username_give']
-    password_receive = request.form['password_give']
-
-    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one(
-        {'username': username_receive, 'password': pw_hash})
-
-    if result is not None:
-        payload = {
-            'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
-    else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
-
-
-@app.route('/sign_up/save', methods=['POST'])
-def sign_up():
-    # 회원가입
-    username_receive = request.form['username_give']
-    password_receive = request.form['password_give']
-
-    # PW해시(암호화)
-    password_hash = hashlib.sha256(
-        password_receive.encode('utf-8')).hexdigest()
-    doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": username_receive,                           # 프로필 이름 기본값은 아이디
-        "profile_pic": "",                                          # 프로필 사진 파일 이름
-        "profile_pic_real": "profile_pics/profile_placeholder.png",  # 프로필 사진 기본 이미지
-        "profile_info": ""                                          # 프로필 한 마디
-    }
-    # DB 저장
-    db.users.insert_one(doc)
-    return jsonify({'result': 'success'})
-
-#  ID중복확인
-
-
-@app.route('/sign_up/check_dup', methods=['POST'])
-def check_dup():
-    username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({"username": username_receive}))
-    return jsonify({'result': 'success', 'exists': exists})
-
-
-# 메인 페이지 상위 15개 전시
-@app.route('/info', methods=['GET'])
-def read_info():
-    all_info = list(db.exhibition.find({}, {'_id': False}))
-    return jsonify({'all_info': all_info})
-
-
-# @app.route('/show')
-# def showshow():
-#     return render_template('/show.html')
-
-@app.route('/show', methods=['GET'])
-def show():
-    shows = list(db.exhibition.find({}, {'_id': False}))
-    return render_template("show.html", shows=shows)
 
 
 if __name__ == '__main__':
