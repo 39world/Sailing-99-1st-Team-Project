d3f0ba5ecac2dbb54d533fc6a4bf52dd502a0767
Sanggu <rkdtkdrn93@naver.com>
2021-03-04 23:07:19 +0900

    배포 버전
    배포 버전

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
 
 SECRET_KEY = 'SPARTA'
 
-client = MongoClient('localhost', 27017)
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@localhost', 27017)
 db = client.dbsparta_plus_week4
 
 #------------------------------------------------------랜딩페이지 index.html------------------------------------------------------
